7650078b75c42ff2ea461c78fb93230304022a9a
Yash <yashdabhade.official@gmail.com>
2025-11-12 01:21:31 +0530

    Refactor KaiserWindow.m for improved readability

--- KaiserWindow.m
+++ KaiserWindow.m
@@ -1,26 +1,36 @@
 close all;
 clc;
-alphap=0.01;
-alphas=44;
-ws=30;
-wp=20;
-wfs=100;
-b=ws-wp;
-wc=0.5*(ws+wp);
-wcr=wc*2*pi/wfs;
-d=(alphas-7.95)/14.36;
-N=ceil((wfs*d/b)+1)
-alpha=(N-1)/2;
-gamma=((0.5842*(alphas-21).^(0.4)+0.07886*(alphas-21)));
-n=0:1:N-1;
-a1=sin(wcr*(n-alpha));
-a2=(pi*(n-alpha));
-hd=a1./a2;
-hd(alpha+1)=0.5;
-wk=kaiser(N,gamma)';
-hn=hd.*wk;
-w=0:1:pi;
-h=freqz(hn,1,w);
-plot(w/pi,20*log10(h))
-xlabel("Normalized frequency\omega/\pi");
-ylabel("magnitude in dB");
+
+alphap = 0.01;
+alphas = 44;
+ws = 30;
+wp = 20;
+wfs = 100;
+
+b  = ws - wp;
+wc = 0.5 * (ws + wp);
+wcr = wc * 2 * pi / wfs;
+
+d = (alphas - 7.95) / 14.36;
+N = ceil((wfs * d / b) + 1)
+
+alpha = (N - 1) / 2;
+
+gamma = (0.5842 * ((alphas - 21)^0.4) + 0.07886 * (alphas - 21));
+
+n = 0:1:N-1;
+a1 = sin(wcr * (n - alpha));
+a2 = pi * (n - alpha);
+
+hd = a1 ./ a2;
+hd(alpha + 1) = 0.5;
+
+wk = kaiser(N, gamma)';
+hn = hd .* wk;
+
+w = 0:1:pi;
+h = freqz(hn, 1, w);
+
+plot(w/pi, 20*log10(h));
+xlabel("Normalized frequency \omega/\pi");
+ylabel("Magnitude in dB");
